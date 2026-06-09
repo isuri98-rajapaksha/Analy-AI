438cf178f1a9ed71dd58907066ea1e4736d47f21
isuri98-rajapaksha <isuriumayangana0812@gmail.com>
2026-06-09 20:06:51 +0530

    Added project screenshots

--- analy-ai-frontend/src/App.jsx
+++ analy-ai-frontend/src/App.jsx
@@ -117,7 +117,7 @@ const newChat = () => {
     <div className="h-screen flex bg-gradient-to-br from-[#1A102B] via-[#24143A] to-[#2D1B47] text-white overflow-hidden">
 
       {/* SIDEBAR */}
-      <div className="w-[270px] bg-gradient-to-b from-[#24143A] to-[#2D1B47] border-r border-[#7B61FF]/20 flex flex-col">
+      <div className="hidden md:flex md:w-[270px] bg-gradient-to-b from-[#24143A] to-[#2D1B47] border-r border-[#7B61FF]/20 flex-col">
         {/* TOP */}
         <div className="p-5 border-b border-[#7B337E]/20">
 
@@ -193,21 +193,29 @@ const newChat = () => {
 
       {/* MAIN CONTENT AREA */}
 <div className="flex-1 flex flex-col bg-[#210635]">
+  <div className="md:hidden flex items-center justify-center py-4 border-b border-[#7B61FF]/20">
+  <h2 className="font-bold text-xl">
+    <span className="text-white">Analy</span>
+    <span className="ml-1 bg-gradient-to-r from-[#7B61FF] to-[#C084FC] bg-clip-text text-transparent">
+      AI
+    </span>
+  </h2>
+</div>
   {/* CHAT AREA */}
 {/* CHAT AREA */}
-<div className="flex-1 overflow-y-auto px-10 py-4">
+<div className="flex-1 overflow-y-auto px-4 md:px-10 py-4">
 
   {/* EMPTY STATE */}
   {messages.length === 0 && !loading && (
     <div className="h-full flex flex-col items-center justify-center text-center">
 
-      <img
-  src="/logo.png"
+     <img
+  src={`${import.meta.env.BASE_URL}logo.png`}
   alt="Analy AI"
-  className="w-[320px] max-w-full object-contain mb-[-10px]"
+  className="w-[220px] md:w-[320px] max-w-full object-contain mb-[-10px]"
 />
 
-     <h1 className="text-4xl font-bold mt-0 whitespace-nowrap">
+     <h1 className="text-2xl md:text-4xl font-bold mt-0 text-center">
 
   <span className="text-white">
     Welcome to
@@ -219,7 +227,7 @@ const newChat = () => {
 
 </h1>
 
-      <p className="text-[#C9B4D4] mt-4 max-w-2xl">
+      <p className="text-[#C9B4D4] mt-4 max-w-2xl text-sm md:text-base px-4">
         Ask about universities, scholarships, visa guidance,
         careers and study opportunities worldwide.
       </p>
@@ -259,7 +267,7 @@ const newChat = () => {
         className={`flex ${msg.type === "user" ? "justify-end" : "justify-start"}`}
       >
     
-        <div className={`max-w-[85%] prose prose-invert px-5 py-4 rounded-2xl text-sm leading-7 shadow-lg 
+        <div className={`max-w-[95%] md:max-w-[85%] prose prose-invert px-5 py-4 rounded-2xl text-sm leading-7 shadow-lg 
           ${msg.type === "user" 
             ? "bg-gradient-to-r from-[#6667AB] to-[#7B337E] rounded-br-md" 
             : "bg-white/5 backdrop-blur-xl border border-[#7B61FF]/20 rounded-bl-md"
@@ -286,7 +294,7 @@ const newChat = () => {
 
  
         {/* INPUT AREA */}
-        <div className="p-6 border-t border-[#7B337E]/20 bg-[#24143A]/60">
+        <div className="p-3 md:p-6 border-t border-[#7B337E]/20 bg-[#24143A]/60">
 
           <div className="max-w-4xl mx-auto bg-[#2D1B47]/20 border border-[#7B61FF]/20 rounded-3xl backdrop-blur-xl p-4 shadow-2xl">
 
@@ -295,7 +303,7 @@ const newChat = () => {
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               placeholder="Tell Analy AI about your study goals..."
-              className="w-full h-18 bg-transparent resize-none outline-none text-[#F5D5E0] placeholder:text-[#C9B4D4] text-[15px]"
+              className="w-full h-18 md:h-18 bg-transparent resize-none outline-none text-[#F5D5E0] placeholder:text-[#C9B4D4] text-[14px] md:text-[15px]"
             />
 
 
@@ -307,10 +315,10 @@ const newChat = () => {
 
 
             {/* BOTTOM BAR */}
-            <div className="flex items-center justify-between mt-4">
+            <div className="flex flex-col md:flex-row gap-3 md:gap-0 items-stretch md:items-center justify-between mt-4">
 
               {/* PDF BUTTON */}
-              <label className="cursor-pointer bg-[#3B1657] hover:bg-[#4a1f6d] transition px-4 py-2 rounded-xl text-sm text-[#F5D5E0]">
+              <label className="w-full md:w-auto bg-gradient-to-r from-[#6667AB] to-[#7B337E] hover:scale-105 hover:opacity-90 active:scale-95 transition px-6 py-2 rounded-xl text-sm font-medium">
                 📎 Upload PDF
                <input
   type="file"
